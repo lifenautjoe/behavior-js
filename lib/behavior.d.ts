e525144ae969986506fd4e3b51800965c4dcdbdb
Joel Hernandez <involvmnt@gmail.com>
2015-11-27 13:25:16 +0100

    Adds the lib directory

--- lib/behavior.d.ts
+++ lib/behavior.d.ts
@@ -1,5 +1,10 @@
 /// <reference path="../typings/tsd.d.ts" />
 /// <reference path="../typings/app.d.ts" />
 declare class Behavior {
+    allowed: boolean;
+    exceptions: string[];
+    constructor(allowed: boolean, exceptions: string);
+    constructor(allowed: boolean, exceptions: string[]);
+    isAllowed(action: string): boolean;
 }
 export = Behavior;
