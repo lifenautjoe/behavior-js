718d71d792fdbbdc3a820add63989997864f90f3
Joel Hernandez <involvmnt@gmail.com>
2015-11-27 13:22:41 +0100

    Adds behavior module implementation

--- src/behavior.ts
+++ src/behavior.ts
@@ -5,8 +5,38 @@
 /// <reference path="../typings/tsd.d.ts" />
 /// <reference path="../typings/app.d.ts" />
 
-class Behavior {
+import _ = require('lodash');
 
+class Behavior {
+    public allowed : boolean;
+    public exceptions : string[];
+
+    constructor(allowed : boolean, exceptions : string);
+    constructor(allowed : boolean, exceptions : string[]);
+    constructor(allowed : boolean, exceptions : any) {
+
+        // Default to always allowed if no argument was given or the given argument is not a boolean
+        this.allowed = (_.isUndefined(allowed) || !_.isBoolean(allowed)) ? true : allowed;
+
+        // If we were given a string, convert it to array
+        if (_.isString(exceptions)) {
+            exceptions = [ exceptions ];
+        }
+
+        // Default to an empty exceptions array if no exceptions argument was given or the given argument is not an array
+        this.exceptions = _.isArray(exceptions) ? exceptions :  [];
+    }
+
+    /**
+     * Checks whether a behavior action is allowed
+     *
+     * @param {string} action
+     * @returns {boolean}
+     */
+    public isAllowed(action : string) {
+        return (this.allowed && !_.includes(this.exceptions, action)) ||
+            (!this.allowed && _.includes(this.exceptions, action));
+    }
 }
 
 export = Behavior;
